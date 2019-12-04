7528fc63b84b48f007fddfb8edfa7a8dfaf089fe
Songbai Yan <colorguitar@hotmail.com>
2019-12-04 17:38:29 +0800

    add test_should_return_x_plus_1_given_given_command_is_M_and_facing_is_E case

--- marsrover/mars_rover.py
+++ marsrover/mars_rover.py
@@ -4,5 +4,7 @@ class MarsRover:
         self.__y = 0
         self.__direction = 'E'
 
-    def run(self):
+    def run(self, command=None):
+        if command == 'M':
+            self.__x += 1
         return '%s %s %s' % (self.__x, self.__y, self.__direction)
--- tests/test_mars_rover.py
+++ tests/test_mars_rover.py
@@ -8,3 +8,8 @@ class TestMarsRover(unittest.TestCase):
         mars_rover = MarsRover()
         status = mars_rover.run()
         self.assertEqual(status, '0 0 E')
+
+    def test_should_return_x_plus_1_given_given_command_is_M_and_facing_is_E(self):
+        mars_rover = MarsRover()
+        status = mars_rover.run('M')
+        self.assertEqual(status, '1 0 E')
